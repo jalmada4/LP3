1d59182bdb82ce2631425179809832b1f07667a4
jalmada4 <aaandraded4.ja@gmail.com>
2024-10-15 08:48:42 -0300

    5-js

---
+++ javascript/jsavanzado/formulario/js/script.js
@@ -0,0 +1,30 @@
+window.addEventListener('load', ()=>{
+
+    $('#nombre').bind('keyup blur', function(){
+        var node = $(this);
+        node.val(node.val().replace(/[^a-z && A-Z]/g,''));
+    });
+
+    $('#apellido').bind('keyup blur', function(){
+        var node = $(this);
+        node.val(node.val().replace(/[^a-z && A-Z]/g,''));
+    });
+
+    $('#ci').bind('keyup blur', function(){
+        var node = $(this);
+        node.val(node.val().replace(/[^0-9]/g,''));
+    });
+
+    document.getElementById('email').addEventListener('input', function(event){
+        campo = event.target;
+        valido = document.getElementById('emailok');
+
+        emailreg = /^[-\w.%+]{1,64}@(?:[A-z0-9-]{1,64}\.){1,50}[A-Z]{2,10}$/i;
+        if(emailreg.test(campo.value)){
+            valido.innerText = "Email válido";
+        }else{
+            valido.innerText = "Email incorrecto!";
+        }
+    });
+
+});
